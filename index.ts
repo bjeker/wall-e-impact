ab7691f7612047f1bcb5867fbd911a6297e49a05
bjeker <rbieke1@gmail.com>
2024-04-24 16:50:29 -0500

    update manifest and plugin

--- index.ts
+++ index.ts
@@ -1,40 +1,19 @@
-import { cpuUsage } from "process";
+async function runPlugin() {
+  const newModel = await new MyCustomPlugin().configure(params);
+  const usage = await newModel.calculate([
+    {
+      timestamp: '2021-01-01T00:00:00Z',
+      duration: '15s',
+      'cpu-util': 34,
+    },
+    {
+      timestamp: '2021-01-01T00:00:15Z',
+      duration: '15s',
+      'cpu-util': 12,
+    },
+  ]);
 
-export type PluginInterface = {
-    execute: (
-      inputs: PluginParams[],
-      config?: Record<string, any>
-      outputs: PluginParams[]
-    ) => PluginParams[];
-    metadata: {
-      kind: string;
-    };
-    [key: string]: any;
-  };
+  console.log(usage);
+}
 
-  // Here's the function definition - notice that global config is passed in here!
-export const Plugin = (globalConfig: YourConfig): PluginInterface => {
-
-    // in here you have access to globalConfig[your-params]
-    
-    }
-
-async function runModel() {
-    const newModel = await new Model().configure(params);
-    const usage = await newModel.calculate([
-        {
-        timestamp: '2021-01-01T00:00:00Z',
-        duration: '15s',
-        'cpu-util': 34,
-        },
-        {
-        timestamp: '2021-01-01T00:00:15Z',
-        duration: '15s',
-        'cpu-util': 12,
-        },
-    ]);
-    
-    console.log(usage);
-    }
-    
-    runModel();
+runPlugin();
