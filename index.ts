e2682e7221c4ec1766b1b35eefbe3f5f50d4733a
bjeker <rbieke1@gmail.com>
2024-04-24 18:03:08 -0500

    update back to before error

--- index.ts
+++ index.ts
@@ -1,19 +1,48 @@
-async function runPlugin() {
-  const newModel = await new MyCustomPlugin().configure(params);
-  const usage = await newModel.calculate([
-    {
-      timestamp: '2021-01-01T00:00:00Z',
-      duration: '15s',
-      'cpu-util': 34,
-    },
-    {
-      timestamp: '2021-01-01T00:00:15Z',
-      duration: '15s',
-      'cpu-util': 12,
-    },
-  ]);
+export const Sum = (globalConfig: SumConfig): PluginInterface => {
+  const errorBuilder = buildErrorMessage(Sum.name);
+  const metadata = {
+    kind: 'execute',
+  };
 
-  console.log(usage);
+  /**
+   * Calculate the sum of each input.
+   */
+  const execute = async (inputs: PluginParams[]): Promise<PluginParams[]> => {
+
+  };
+
+  return {
+    metadata,
+    execute,
+  };
+
 }
 
-runPlugin();
+  /**
+   * Calculate the sum of each input.
+   */
+  const execute = async (inputs: PluginParams[]): Promise<PluginParams[]> => {
+    const inputParameters = globalConfig['input-parameters'];
+    const outputParameter = globalConfig['output-parameter'];
+
+    return inputs.map(input => {
+      return {
+        ...input,
+        [outputParameter]: calculateSum(input, inputParameters),
+      };
+    });
+
+  return {
+    metadata,
+    execute,
+  };
+}
+
+  /**
+   * Calculates the sum of the energy components.
+   */
+  const calculateSum = (input: PluginParams, inputParameters: string[]) =>
+    inputParameters.reduce(
+      (accumulator, metricToSum) => accumulator + input[metricToSum],
+      0
+    );
