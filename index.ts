5292a79cd26faa7d7fec30bde050e959cc5fc302
bjeker <rbieke1@gmail.com>
2024-04-28 21:27:54 -0500

    update manifest and index to work

--- index.ts
+++ index.ts
@@ -1,48 +1,24 @@
-export const Sum = (globalConfig: SumConfig): PluginInterface => {
-  const errorBuilder = buildErrorMessage(Sum.name);
+const Copy = (globalConfig) => {
   const metadata = {
     kind: 'execute',
   };
 
-  /**
-   * Calculate the sum of each input.
-   */
-  const execute = async (inputs: PluginParams[]): Promise<PluginParams[]> => {
-
+  const execute = async (inputs, config) => {
+    //console.log(inputs);
+    //console.log("Energy used was :", inputs[3])
+    if (inputs[5]["engine"] === "unity") {
+      console.log("Unity has 16% less power usage and 22% less gpu usage")
+    }
+    else if (inputs[5]["engine"] === "Unreal Engine")
+      {
+        console.log("Unreal engine has 16% more power usage and 22% more gpu usage")
+      }
   };
-
+ 
   return {
     metadata,
     execute,
   };
+};
 
-}
-
-  /**
-   * Calculate the sum of each input.
-   */
-  const execute = async (inputs: PluginParams[]): Promise<PluginParams[]> => {
-    const inputParameters = globalConfig['input-parameters'];
-    const outputParameter = globalConfig['output-parameter'];
-
-    return inputs.map(input => {
-      return {
-        ...input,
-        [outputParameter]: calculateSum(input, inputParameters),
-      };
-    });
-
-  return {
-    metadata,
-    execute,
-  };
-}
-
-  /**
-   * Calculates the sum of the energy components.
-   */
-  const calculateSum = (input: PluginParams, inputParameters: string[]) =>
-    inputParameters.reduce(
-      (accumulator, metricToSum) => accumulator + input[metricToSum],
-      0
-    );
+exports.Copy = Copy;
